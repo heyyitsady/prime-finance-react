5a014daf3e25cc1f5adf36b61a3e26fa9cf126ad
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-10 22:32:11 +0530

    feat: write Cards component

---
+++ src/components/Cards.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Single from "../assets/single.png";
+import Double from "../assets/double.png";
+import Triple from "../assets/triple.png";
+
+import Card from "./Card";
+
+const details = [
+  {
+    id: 1,
+    user: "Single",
+    img: Single,
+    price: "$149",
+    p1: "500 GB Storage",
+    p2: "1 Granted User",
+    p3: "Send upto 2 GB",
+  },
+  {
+    id: 2,
+    user: "Two",
+    img: Double,
+    price: "$249",
+    p1: "500 GB Storage",
+    p2: "1 Granted User",
+    p3: "Send upto 2 GB",
+    mid: true,
+  },
+  {
+    id: 3,
+    user: "Three",
+    img: Triple,
+    price: "$329",
+    p1: "500 GB Storage",
+    p2: "1 Granted User",
+    p3: "Send upto 2 GB",
+  },
+];
+
+const Cards = () => {
+  return (
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        {details.map(({ id, ...otherDetails }) => (
+          <Card key={id} {...otherDetails} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
