370aa6de5cbea491ba90a0934546d00c731ebde7
Atharva Pardeshi <atharva.exe@gmail.com>
2022-10-16 18:56:17 +0530

    feat: change footer links to actually be links rather than just text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,37 +32,69 @@ const Footer = () => {
         <div>
           <h6 className="font-medium text-gray-400">Solutions</h6>
           <ul>
-            <li className="py-2 text-sm">Analytics</li>
-            <li className="py-2 text-sm">Marketing</li>
-            <li className="py-2 text-sm">Commerce</li>
-            <li className="py-2 text-sm">Insights</li>
+            <li className="py-2 text-sm">
+              <a href="/">Analytics</a>
+            </li>
+            <li className="py-2 text-sm">
+              <a href="/">Marketing</a>
+            </li>
+            <li className="py-2 text-sm">
+              <a href="/">Commerce</a>
+            </li>
+            <li className="py-2 text-sm">
+              <a href="/">Insights</a>
+            </li>
           </ul>
         </div>
         <div>
           <h6 className="font-medium text-gray-400">Support</h6>
           <ul>
-            <li className="py-2 text-sm">Pricing</li>
-            <li className="py-2 text-sm">Documentation</li>
-            <li className="py-2 text-sm">Guides</li>
-            <li className="py-2 text-sm">API Status</li>
+            <li className="py-2 text-sm">
+              <a href="/">Pricing</a>
+            </li>
+            <li className="py-2 text-sm">
+              <a href="/">Documentation</a>
+            </li>
+            <li className="py-2 text-sm">
+              <a href="/">Guides</a>
+            </li>
+            <li className="py-2 text-sm">
+              <a href="">API Status</a>
+            </li>
           </ul>
         </div>
         <div>
           <h6 className="font-medium text-gray-400">Company</h6>
           <ul>
-            <li className="py-2 text-sm">About</li>
-            <li className="py-2 text-sm">Blog</li>
-            <li className="py-2 text-sm">Press</li>
-            <li className="py-2 text-sm">Jobs</li>
-            <li className="py-2 text-sm">Careers</li>
+            <li className="py-2 text-sm">
+              <a href="/">About</a>
+            </li>
+            <li className="py-2 text-sm">
+              <a href="/">Blog</a>
+            </li>
+            <li className="py-2 text-sm">
+              <a href="/">Press</a>
+            </li>
+            <li className="py-2 text-sm">
+              <a href="/">Jobs</a>
+            </li>
+            <li className="py-2 text-sm">
+              <a href="/">Careers</a>
+            </li>
           </ul>
         </div>
         <div>
           <h6 className="font-medium text-gray-400">Legal</h6>
           <ul>
-            <li className="py-2 text-sm">Claim</li>
-            <li className="py-2 text-sm">Policy</li>
-            <li className="py-2 text-sm">Terms</li>
+            <li className="py-2 text-sm">
+              <a href="/">Claim</a>
+            </li>
+            <li className="py-2 text-sm">
+              <a href="/">Policy</a>
+            </li>
+            <li className="py-2 text-sm">
+              <a href="/">Terms</a>
+            </li>
           </ul>
         </div>
       </div>
